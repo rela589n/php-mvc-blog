7a6c01d7b60f911b7c5671f50d970e45ab76edff
rela589n <zsidelnik@gmail.com>
2019-11-25 20:58:34 +0200

    Texts Logic into service. Model works only with database

--- core/services/Texts.php
+++ core/services/Texts.php
@@ -3,8 +3,78 @@
 
 namespace core\services;
 
+use core\exceptions\IncorrectDataException;
+use core\Validator;
+use model\Texts as TextsModel;
 
-class TextsService
+class Texts extends Base
 {
+    const TEXT_VALUE_PREVIEW_MAX_LENGTH = 60;
+    const INVALID_PARAMS_GIVEN = 'Передано невалидные данные';
+
+    private const SCHEMA = [
+        'alias' => [
+            'type' => 'string',
+            'max_length' => 64
+        ],
+        'name' => [
+            'type' => 'string',
+            'min_length' => 4,
+            'max_length' => 128,
+        ],
+        'value' => [
+            'type' => 'string',
+            'max_length' => 256,
+        ]
+    ];
+
+    private $textsModel;
+
+    public function __construct(TextsModel $textsModel, Validator $validator)
+    {
+        $this->textsModel = $textsModel;
+        parent::__construct($validator);
+        $this->validator->setSchema(self::SCHEMA);
+    }
+
+    public function getText(string $alias)
+    {
+        return $this->textsModel->getById($alias)['value'];
+    }
+
+    public function alterText(array $params)
+    {
+        $this->validator->validateByFields([
+            'name' => $params['name'],
+            'value' => $params['value'],
+            $this->textsModel->getIdAlias() => $params['alias']
+        ]);
+
+        if (!$this->validator->success()) {
+            throw new IncorrectDataException($this->validator->errors, self::INVALID_PARAMS_GIVEN);
+        }
+
+        $clear = $this->validator->clear;
+
+        return $this->textsModel->update(
+            $clear['name'],
+            $clear['value'],
+            $clear[$this->textsModel->getIdAlias()]
+        );
+    }
+
+    public function getPreview(array $text)
+    {
+        $text['value'] = substr($text['value'], 0, self::TEXT_VALUE_PREVIEW_MAX_LENGTH) . '...';
+        return $text;
+    }
+
+    public function getPreviews(array $texts)
+    {
+        return array_map(function ($text) {
+            return $this->getPreview($text);
+        }, $texts);
+    }
+
 
 }
--- model/Texts.php
+++ model/Texts.php
@@ -9,62 +9,26 @@ use core\Validator;
 
 class Texts extends Base
 {
-    const TEXT_VALUE_PREVIEW_MAX_LENGTH = 60;
-
-    private const SCHEMA = [
-        'alias' => [
-            'type' => 'string',
-            'max_length' => 64
-        ],
-        'name' => [
-            'type' => 'string',
-            'min_length' => 4,
-            'max_length' => 128,
-        ],
-        'value' => [
-            'type' => 'string',
-            'max_length' => 256,
-        ]
-    ];
-
-    public function __construct(DBDriverInterface $db, Validator $validator)
+    public function __construct(DBDriverInterface $db)
     {
-        parent::__construct($db, $validator, "dashboard_texts", "alias");
-        $this->validator->setSchema(self::SCHEMA);
+        parent::__construct($db, 'dashboard_texts', "alias");
     }
 
-    public function getOne(string $alias)
+    public function update(string $name, string $value, string $alias, string $oldAlias = null)
     {
-        return $this->getById($alias)['value'];
-    }
+        $oldAlias = $oldAlias ?? $alias;
 
-    public function update(string $name, string $value, string $alias)
-    {
         return $this->db->update(
             $this->tableName,
             [
                 'name' => $name,
                 'value' => $value,
-                'alias' => $alias
+                $this->idAlias => $alias
             ],
-            "{$this->idAlias} = :alias",
+            "{$this->idAlias} = :whereAlias",
             [
-                'alias' => $alias
+                'whereAlias' => $oldAlias
             ]
         );
-
-    }
-
-    public function getPreview(array $text)
-    {
-        $text['value'] = substr($text['value'], 0, self::TEXT_VALUE_PREVIEW_MAX_LENGTH) . '...';
-        return $text;
-    }
-
-    public function getPreviews(array $texts)
-    {
-        return array_map(function ($text) {
-            return self::getPreview($text);
-        }, $texts);
     }
 }
